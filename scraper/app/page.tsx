121f2e220f7f78236624ee62a8f1f50fb1864ef1
Petrus <petrusheya@gmail.com>
2024-07-01 16:42:34 +0200

    Init commit

---
+++ scraper/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen w-full flex flex-col items-center justify-center p-40 pt-5 bg-grid-white/[0.1] bg-black text-white">
+      <h1 className="text-3xl sm:text-7xl tracking-tighter font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 pb-8">
+        Leads Scraper
+      </h1>
+      <section className="flex gap-2">
+        <div className="flex flex gap-5">
+          <input type="text" className="w-72 h-10 utline-none focus:outline-none rounded-md p-2 bg-neutral-200 text-black"/>
+          <input type="text"  className="w-72 h-10 utline-none focus:outline-none rounded-md p-2 bg-neutral-200 text-black"  />
+        </div> 
+        <div>
+          <button className="bg-black ring-2 flex items-center justify-center ring-white shadow-md shadow-neutral-700  text-center p-3 px-5 text-white w-max rounded-md h-10 hover:translate-y-1 transition ">Search</button>
+        </div>
+      </section> 
+      <section>
+
+      </section>
+    </main>
+  );
+}
