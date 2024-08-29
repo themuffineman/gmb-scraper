2135ba08ef8b09983c27241952eef118d6b09b8c
Petrus <petrusheya@gmail.com>
2024-08-29 17:05:38 +0200

    Added meta tags

--- scraper/app/layout.tsx
+++ scraper/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <meta property="og:title" content="Pendora" />
+        <meta property="og:description" content="Helping web developers and designers filter out the noise and find their ideal client" />
+        <meta property="og:image" content="https://i.postimg.cc/h4NLFv9w/pendora-preview.png" />
+        <meta property="og:url" content="https://pendora.org/" />
+      </Head>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   );
 }
